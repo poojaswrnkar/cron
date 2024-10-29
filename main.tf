c2b65e8695ef10579923c7c7411bdab2506e01a8
poojaswrnkar <118523295+poojaswrnkar@users.noreply.github.com>
2024-10-29 12:06:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-  region = var.region  # Using the region variable
+  region     = var.region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI, choose as per region
   instance_type = var.instance_type
+
   tags = {
     Name        = var.instance_name
     Environment = var.environment
