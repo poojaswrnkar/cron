07640a4bc4a510529b554d11f0b50b3eec6ad4b1
poojaswrnkar <118523295+poojaswrnkar@users.noreply.github.com>
2024-11-06 12:27:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"  # Change to your desired region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 variable "instance_name" {
